26c847cc36af08c05298a0628e21635f8bb69a3b
Sergey Barkhatov <sergeybarkhat@yandex.ru>
2023-05-28 16:21:50 +0200

    Отправить проект Mesto

---
+++ scripts/index.js
@@ -0,0 +1,30 @@
+let content = document.querySelector('.content');
+let editButton = content.querySelector('.profile__edit-button');
+let popup = content.querySelector('.popup');
+let popupForm = content.querySelector('.popup__form');
+let popupInputName = content.querySelector('.popup__input_type_name');
+let popupInputAbout = content.querySelector('.popup__input_type_about');
+let profileName = content.querySelector('.profile__name');
+let profileAbout = content.querySelector('.profile__about');
+let closeButton = content.querySelector('.popup__close');
+
+function editProfile() {
+  popup.classList.add('popup_visibility_true');
+  popupInputName.value = profileName.textContent;
+  popupInputAbout.value = profileAbout.textContent;  
+}
+
+function closeEdit() {
+  popup.classList.remove('popup_visibility_true');
+} 
+
+function saveEdit(evt) {
+  evt.preventDefault();
+  profileName.textContent = popupInputName.value;
+  profileAbout.textContent = popupInputAbout.value;
+  closeEdit();
+}
+
+editButton.addEventListener('click', editProfile);
+closeButton.addEventListener('click', closeEdit);
+popupForm.addEventListener('submit', saveEdit);
